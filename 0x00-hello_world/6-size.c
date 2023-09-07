f75c2c6977e9884b26adb801baab99bddb4d4c76
MABDesigns <bdeiramostephamine@pm.me>
2023-09-07 11:43:59 -0700

    Added a c script that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Beginning
- * 
+ *
  * Description: task 6 0x00-hello_world
  *
  * Return: 0
@@ -17,7 +17,7 @@ float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
