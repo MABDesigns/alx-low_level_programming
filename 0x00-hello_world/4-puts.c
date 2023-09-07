64eee37bfdd4ad913f8a5749b4f82263b42e3a27
MABDesigns <bdeiramostephamine@pm.me>
2023-09-07 10:12:07 -0700

    Added a c script that prints exactly Programming is like building a multilingual puzzle.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
+/*
+ * Task 4 , printing Programming is like building a multiligual puzzle without printf
+ *
+ */
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
