3cefdc1233f0f29ba03546babdb43708b3c1843d
MABDesigns <bdeiramostephamine@pm.me>
2023-09-07 10:14:37 -0700

    Added c script that prints exactly Programming is like building a multilingual puzzle.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * Task 4 , printing Programming is like building a multiligual puzzle without printf
+/**
+ * Task 4: printing Programming is like building a multiligual puzzle without printf
  *
  */
 int main(void)
