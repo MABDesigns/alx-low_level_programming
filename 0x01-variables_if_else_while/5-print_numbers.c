a67021b3dcd7b5c499a1cd9fa513a5ca5cbb84a2
MABDesigns <bdeiramostephamine@pm.me>
2023-09-08 15:36:51 -0700

    Added a c script that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Beginning
+ * Description: task5 0x01
+ * Return: 0
+ */
+int main(void)
+{
+int n = 10;
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%i\n", i);
+}
+return (0);
+}
