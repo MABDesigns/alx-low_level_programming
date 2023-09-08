4d2099c8446b6078567f6ec715496fff3c7e179f
MABDesigns <bdeiramostephamine@pm.me>
2023-09-08 16:34:46 -0700

    Added a c script that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - beginning
+ * Description: 0x01
+ * Return: 0
+ */
+int main(void)
+{
+char test;
+for (test = 'z'; test >= 'a'; test--)
+{
+putchar(test);
+}
+putchar('\n');
+return (0);
+}
