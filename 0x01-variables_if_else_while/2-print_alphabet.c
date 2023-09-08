3082e92613eaee156da93e9613a7ba7be425f1ca
MABDesigns <bdeiramostephamine@pm.me>
2023-09-08 13:42:59 -0700

    Added a c script that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Beginning
+ *
+ * Description: Task 2 0x01
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
