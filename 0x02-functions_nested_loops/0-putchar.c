1a175a1a83f8a0b89dcb39319908e34ef447ed71
MABDesigns <bdeiramostephamine@pm.me>
2023-09-12 06:34:46 -0700

    Added a c script that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "header.h"
+/**
+ * main - beginning
+ * Description: task 1 0x02
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
