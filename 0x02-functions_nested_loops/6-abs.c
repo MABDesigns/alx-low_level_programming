c0c97d097ad8c9a8d0309d27ed3b234e485ea62b
MABDesigns <bdeiramostephamine@pm.me>
2023-09-12 16:55:50 -0700

    Added a c script that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - function
+ * @r: intput
+ * Description: task 6 0x02
+ * Return: Absolute value
+ */
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (r * -1);
+}
