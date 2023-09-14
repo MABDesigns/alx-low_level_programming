f6be4d2d21c1d70bb995381437edc03e9e2d44be
MABDesigns <bdeira-mostepha-amine@univ-eltarf.dz>
2023-09-14 16:49:41 -0700

    Added a c function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function
+ * @x: first input
+ * @y: second input
+ * Return: result
+ */
+int mul(int x, int y)
+{
+return (x * y);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int x, int y);
 
 
 #endif
